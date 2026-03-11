807024eff399851f225feb1b0114315516e5d68e
om2309 <23anuragsharma@gmail.com>
2026-03-11 21:16:35 +0530

    file creation

---
+++ basic/main.py
@@ -0,0 +1,23 @@
+# print("Hello, World!")
+# print(f"Current year: {2024}")
+
+# name = input("What is your name? ")
+# print(f"Nice to meet you, {name}!")
+
+# for i in range(5):
+#     print(i)
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# print(greet(name))
+
+# file create
+# with open("example.txt", "w") as file:
+#     file.write("This is an example file.\n")
+#     file.write("It contains some text.")
+
+# file read
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
