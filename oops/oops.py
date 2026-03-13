f746c223271f73a18056bd64e6a9cc4041617cda
om2309 <23anuragsharma@gmail.com>
2026-03-13 23:42:29 +0530

    CLass basic

--- oops/oops.py
+++ oops/oops.py
@@ -14,16 +14,35 @@
 # user1.info()
 
 
-class Calculator:
+# class Calculator:
 
-    def add(self, a, b):
-        return a + b
+#     def add(self, a, b):
+#         return a + b
 
-    def multiply(self, a, b):
-        return a * b
+#     def multiply(self, a, b):
+#         return a * b
 
 
-calc = Calculator()
+# calc = Calculator()
 
-print(calc.add(5, 3))
-print(calc.multiply(4, 2))
+# print(calc.add(5, 3))
+# print(calc.multiply(4, 2))
+
+
+class BankAccount:
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def showBalance(self):
+        print("Balance:", self.balance)
+
+
+acc = BankAccount("Anurag", 1000)
+
+acc.deposit(500)
+acc.showBalance()
