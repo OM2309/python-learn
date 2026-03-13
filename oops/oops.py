08a4102247b51b4c3466be34baa44f9f17c30826
om2309 <23anuragsharma@gmail.com>
2026-03-13 23:33:58 +0530

    CLass basic

--- oops/oops.py
+++ oops/oops.py
@@ -1,1 +1,14 @@
-# object oriented programming
+class Basic:
+
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def info(self):
+        print("Name:", self.name)
+        print("Role:", self.role)
+
+
+user1 = Basic("Anurag", "Developer")
+
+user1.info()
