8beaab8c9a8dbf25731c666936b91dc1e4e6764f
om2309 <23anuragsharma@gmail.com>
2026-03-13 23:41:55 +0530

    CLass basic

--- oops/oops.py
+++ oops/oops.py
@@ -1,14 +1,29 @@
-class Basic:
+# class Basic:
 
-    def __init__(self, name, role):
-        self.name = name
-        self.role = role
+#     def __init__(self, name, role):
+#         self.name = name
+#         self.role = role
 
-    def info(self):
-        print("Name:", self.name)
-        print("Role:", self.role)
+#     def info(self):
+#         print("Name:", self.name)
+#         print("Role:", self.role)
 
 
-user1 = Basic("Anurag", "Developer")
+# user1 = Basic("Anurag", "Developer")
 
-user1.info()
+# user1.info()
+
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def multiply(self, a, b):
+        return a * b
+
+
+calc = Calculator()
+
+print(calc.add(5, 3))
+print(calc.multiply(4, 2))
